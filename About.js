8211058ef650605b737723be591078fdf292c397
22231363-lang <22231363@students.liu.edu.lb>
2025-11-24 21:17:59 +0200

    Add About component with company description

---
+++ About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="card p-3">
+      <h2>About Us</h2>
+      <p>JAYJAY brings tasty meals with fast delivery.</p>
+    </div>
+  );
+}
